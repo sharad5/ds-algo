fe5c9ecec4c7017ae0b4bc609494d9a1d235a427
Jyotman <jyotman94@gmail.com>
2016-02-06 23:01:46 +0530

    Link List

--- src/jyotman94/linkedlist/LinkedList.java
+++ src/jyotman94/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package jyotman94.linkedlist;
 
+import java.util.Stack;
+
 public class LinkedList {
 
 	Node root;
@@ -15,21 +17,123 @@ public class LinkedList {
 	}
 
 	public static void main(String... args) {
-		LinkedList ll = new LinkedList(1);
-		ll.add(2);
-		ll.add(3);
-		ll.add(4);
-		ll.add(5);
-		ll.add(6);
+		LinkedList ll = new LinkedList(7);
+		ll.add(1);
+		ll.add(1);
 		ll.add(7);
 
 		ll.printList();
-		ll.reverseInFixedGroups(2);
-		ll.printList();
+		System.out.println(ll.isPalindrome());
+	}
+
+	// Q1
+	public void removeDuplicates() {
+
+	}
+
+	// Q2
+	public void deleteMiddleAccessNode(Node middle) {
+		middle.data = middle.next.data;
+		middle.next = middle.next.next;
+	}
+
+	// Q3
+	public static LinkedList addNumberedLists(Node head1, Node head2) {
+		Node list1 = head1, list2 = head2;
+		int ones, carry;
+		int sum = head1.data + head2.data;
+		if (sum >= 10) {
+			ones = Integer.parseInt(String.valueOf(sum).substring(1, 2));
+			carry = Integer.parseInt(String.valueOf(sum).substring(0, 1));
+		} else {
+			ones = sum;
+			carry = 0;
+		}
+		LinkedList ans = new LinkedList(ones);
+		list1 = list1.next;
+		list2 = list2.next;
+
+		while (list1 != null && list2 != null) {
+			sum = list1.data + list2.data + carry;
+			if (sum >= 10) {
+				ones = Integer.parseInt(String.valueOf(sum).substring(1, 2));
+				carry = Integer.parseInt(String.valueOf(sum).substring(0, 1));
+			} else {
+				ones = sum;
+				carry = 0;
+			}
+			ans.add(ones);
+			list1 = list1.next;
+			list2 = list2.next;
+		}
+
+		while (list1 != null) {
+			sum = list1.data + carry;
+			if (sum >= 10) {
+				ones = Integer.parseInt(String.valueOf(sum).substring(1, 2));
+				carry = Integer.parseInt(String.valueOf(sum).substring(0, 1));
+			} else {
+				ones = sum;
+				carry = 0;
+			}
+			ans.add(ones);
+			list1 = list1.next;
+		}
+
+		while (list2 != null) {
+			sum = list2.data + carry;
+			if (sum >= 10) {
+				ones = Integer.parseInt(String.valueOf(sum).substring(1, 2));
+				carry = Integer.parseInt(String.valueOf(sum).substring(0, 1));
+			} else {
+				ones = sum;
+				carry = 0;
+			}
+			ans.add(ones);
+			list2 = list2.next;
+		}
+
+		if (carry != 0)
+			ans.add(carry);
+
+		ans.reverseList();
+		return ans;
+	}
+
+	// Q4
+	public static LinkedList addNumberedList2(LinkedList list1, LinkedList list2) {
+		list1.reverseList();
+		list2.reverseList();
+		LinkedList ans = addNumberedLists(list1.root, list2.root);
+		list1.reverseList();
+		list2.reverseList();
+		ans.reverseList();
+		return ans;
+	}
+
+	// Q6
+	public boolean isPalindrome(){
+		Node temp = root;
+		int length = this.length();
+		Stack<Integer> stack = new Stack<>();
+		for(int i = 0; i < length / 2; i++){
+			stack.push(temp.data);
+			temp = temp.next;
+		}
+		
+		//For odd number of nodes
+		if(length % 2 == 1)
+			temp = temp.next;
+		
+		for(int i = 0; i < length / 2; i++){
+			if(stack.pop() != temp.data)
+				return false;
+			temp = temp.next;
+		}
+		return true;
 	}
 
 	// Q7
-	// returns root
 	public void swapNode(int a, int b) {
 		if (root == null)
 			return;
@@ -90,6 +194,27 @@ public class LinkedList {
 				break;
 			prev = null;
 		}
+	}
+
+	public void reverseList() {
+		Node prev = null, curr = root, next;
+		while (curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		root = prev;
+	}
+
+	public int length() {
+		int length = 0;
+		Node temp = root;
+		while (temp != null) {
+			length++;
+			temp = temp.next;
+		}
+		return length;
 	}
 
 	public void printList() {
